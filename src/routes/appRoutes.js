68a3b7a9c3544bb5f7d1fe25426bc9b793883661
Ekoramadhan13 <enugraha411@gmail.com>
2026-03-09 07:40:01 +0700

    menambahkan routing untuk manajemen aplikasi

---
+++ src/routes/appRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const appController = require('../controllers/appController');
+const { isAuthenticated } = require('../middleware/authMiddleware');
+const { checkRole } = require('../middleware/roleMiddleware');
+
+router.use(isAuthenticated);
+
+router.get('/', appController.index);
+router.get('/create', checkRole('tim_leader', 'admin'), appController.create);
+router.post('/', checkRole('tim_leader', 'admin'), appController.store);
+router.get('/:id', appController.show);
+router.get('/:id/edit', checkRole('tim_leader', 'admin'), appController.edit);
+router.put('/:id', checkRole('tim_leader', 'admin'), appController.update);
+router.delete('/:id', checkRole('admin'), appController.destroy);
+router.post('/:id/assign', checkRole('ketua_tester', 'admin'), appController.assignTester);
+router.post('/use-case/store', checkRole('tim_leader', 'business_analyst', 'admin'), appController.storeUseCase);
+
+module.exports = router;
